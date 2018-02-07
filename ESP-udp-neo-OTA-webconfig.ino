097c78f5425de33dce2012ed090d2dd514e79fd2
Rubén Arranz Santos <info@tupresenciaonline.com>
2018-02-07 20:33:00 +0100

    Update ESP-udp-neo-OTA-webconfig.ino
    Se agrega control Web de los leds independientemente de Hyperion.

--- ESP-udp-neo-OTA-webconfig.ino
+++ ESP-udp-neo-OTA-webconfig.ino
@@ -15,8 +15,6 @@ WiFi WS2812 LED strip controller
 - mDNS-SD service advertisement
 - HTTP based WiFi configuration
 
-
-
 ASCIIPort protocol:
 Lrgb\n  
 LLrrggbb\n
@@ -35,13 +33,13 @@ no parsing, simply send RGBRGBRGBRGB to set a string of LEDs from #0
 
 RAW Delta protocol
 [deleted....]
-
 */
 
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
-#include <ESP8266mDNS.h>
 #include <NeoPixelBus.h>
+#include <Adafruit_NeoPixel.h>
+#include <string.h>
 #include <ArduinoOTA.h>
 
 // httpupdate includes
@@ -52,12 +50,9 @@ RAW Delta protocol
 
 // wifimanager includes
 #include <DNSServer.h>
-#include <ESP8266WebServer.h>
 #include "WiFiManager.h"          //https://github.com/tzapu/WiFiManager
 
 #define THIS_FILE (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
-
-
 
 ESP8266WebServer httpServer(80);
 ESP8266HTTPUpdateServer httpUpdater;
@@ -74,7 +69,67 @@ unsigned long RAW_ctr = 0;
 // NeoPixel defines.
 // NOTE!!! NeoPin is IGNORED by all but the NeoEsp8266BitBang800KbpsMethod method
 #define NeoPin 3
-#define PixelCount 350
+#define PixelCount 82 // Número total de leds.
+
+//#define CURRENT_MAX 2500 // Corriente máxima ofrecida por la fuente de alimentación (mA).
+String BRIGHTNESS;
+String LEDvalue; // Variable para utilizar en Serial TO RGBW 
+
+/* INICIO: Serial To RGBW
+Este boceto convierte un StringSerial recibido en un valor de color de 32 bits para usar con la biblioteca Adafruit NeoPixel.
+Resultado visto por el serial. Para verlo por el Serial se ha de añadir código al boceto.
+Ref. Código: https://gist.github.com/mnemocron/e6ec7ce9e6ec0fff6ef7f5147fa5dedc
+HEX to RGBW
+type in a HEX color string (RRGGBB or WWRRGBB - no '0x')
+ff2005
+W  0
+R 255
+G 32
+B 5
+----------
+000008
+W 0
+R 0
+G 0
+B 8
+----------
+*/
+class NeoPixel : public Adafruit_NeoPixel
+{
+  private:
+  uint32_t power(uint8_t p_base, uint8_t p_exp){
+    uint32_t pow_ret = 1;
+    for(int p_ctr = 0; p_ctr < p_exp; p_ctr ++){
+      pow_ret *= p_base;
+    }
+    return pow_ret;
+  }
+
+  public:
+  NeoPixel(uint16_t pixels, uint8_t pin, uint8_t type):Adafruit_NeoPixel(pixels, pin, type){};
+    
+  // Convert RGB / RGBW string into packed 32-bit WRGB color.
+  // string format: "RRGGBB" or "RRGGBBWW (no 0x)
+  uint32_t Color( String htoi_str ){
+    char itoh_lower[18] = {"0123456789abcdef"};       // 16 chars + null
+    char itoh_upper[18] = {"0123456789ABCDEF"};       // 16 chars + null
+    uint32_t htoi_number = 0;
+    for(uint8_t index = 0; index < htoi_str.length(); index ++){        // each hex number in String
+      for(uint8_t val = 0; val < 16; val ++){                           // each possible value
+        if (  htoi_str[htoi_str.length() -index -1] == itoh_lower[val] ||
+              htoi_str[htoi_str.length() -index -1] == itoh_upper[val] ){
+          if(val > 0){
+            // htoi_number += val * power(16, index);
+            htoi_number += (uint32_t)( val * (power(16.0, index) + 0.01) );
+          }
+        }
+      }
+    }
+    return (uint32_t)(htoi_number);
+  }
+};
+NeoPixel pixeles(PixelCount, NeoPin, NEO_GRB + NEO_KHZ800);
+/* FIN: Serial To RGBW */
 
 // know methods: {UseBigBang, UseUart, UseAsyncUart, UseDma}
 #define UseDma
@@ -105,9 +160,6 @@ unsigned long RAW_ctr = 0;
 #else
   #error Neo Method is not known or defined. know methods: {UseBigBang, UseUart, UseAsyncUart, UseDma}
 #endif
-
-
-
 
 // struct and object to support direct element access
 struct GrbPixel {
@@ -154,12 +206,9 @@ char name[] = "9px_0000.bmp";       // filename convention (will auto-increment)
 const int imgSize = w*h;
 int px[w*h];                        // actual pixel data (grayscale - added programatically below)
 
-
-
 // A UDP instance to let us send and receive packets over UDP
 WiFiUDP ASCIIudp;
 WiFiUDP RAWudp;
-
 
 void rainbow_flash(int brightness ) {
   int rainbows[6][3] = {
@@ -183,11 +232,12 @@ void rainbow_flash(int brightness ) {
 
 }
 void setup() {
+  LEDvalue = "";
 
-  Serial.begin(115200);
+  Serial.begin(2000000);
 
   strip.Begin();
-
+  
   rainbow_flash(startup_brite);
   
   fill_and_show (startup_brite,0,0); // red for on, not ready
@@ -212,7 +262,7 @@ void setup() {
 
   fill_and_show( 0,startup_brite,0); // green is good
 
-  Serial.println ("Start OTA server.");
+  Serial.println ("Servidor OTA iniciado.");
   ArduinoOTA.setHostname((const char *)my_hostname.c_str());
   ArduinoOTA.begin();
 
@@ -282,12 +332,13 @@ void setup_http_server(){
   httpServer.begin();
 
   MDNS.addService("http", "tcp", 80);
-  Serial.printf("HTTP Server started. Open http://%s.local/update in your browser\n", my_hostname.c_str());
+  Serial.printf("Servidor HTTP iniciado. Abre http://%s.local/update en tu navegador\n", my_hostname.c_str());
 
+  httpServer.on ( "/", []() {handleRoot();} );
+  
   httpServer.on("/bitmap", [](){
     send_bmp();
   });
-
 
   httpServer.on("/screen", [](){
     String message = "<html><head><title>WS2812 LED Receiver</title></head><body>";
@@ -297,7 +348,6 @@ void setup_http_server(){
     httpServer.send(200, "text/html", message);
   });
 
-  
   httpServer.on("/status", [](){
     long int secs = (extended_mill/1000);
     long int mins = (secs/60);
@@ -325,9 +375,8 @@ void setup_http_server(){
     message += "<TR><TH>ascii packets<TD>"; message += ASCII_ctr;
 
     message += "</TABLE>\r\n";
-    
-    message += "</body></html>";
-    httpServer.send(200, "text/html", message);
+
+    httpServer.send(200, "text/html", message); 
   });
 }
 
@@ -352,6 +401,152 @@ void print_leds_table(String & msg) {
   msg += "\r\n</TABLE>\r\n";
 }
 
+void handleRoot() {
+  //digitalWrite ( led, 1 );
+  String out = "<html>\r\n<head>\r\n";
+  out += "<link rel='stylesheet' href='//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css'>\r\n";
+  out += "<style type=\"text/css\">\r\n";
+  out += "#custom-handle {width:3em; height:2.6em; /*top:50%;*/ margin-top:-.8em; text-align:center; line-height:2.6em; font-size:1.5em;}\r\n";
+  out += "#slider {width:55%; position:absolute; bottom:14.5%; left:16%;}\r\n";
+  out += ".boton:hover, .jscolor:hover {cursor:pointer}\r\n";
+  out += "</style>\r\n";
+
+  out += "<script src='https://code.jquery.com/jquery-1.12.4.js'></script>\r\n";
+  out += "<script src='https://code.jquery.com/ui/1.12.1/jquery-ui.js'></script>\r\n";
+  //out += "<script src='https://cdnjs.cloudflare.com/ajax/libs/jqueryui-touch-punch/0.2.3/jquery.ui.touch-punch.min.js'>\r\n"; // ¡Incompatible con jscolor!
+  out += "<script src='https://cdnjs.cloudflare.com/ajax/libs/jscolor/2.0.4/jscolor.min.js'></script>\r\n";
+  
+  out += "<script>\n\r";
+  out += "$(window).on('load',function(){\r\n";
+  out += "$('#valorBrillo').val('255');\r\n";
+  out += "if(";
+  out += httpServer.args();
+  out += " == 0){\r\n";
+  out += "$('.Args').css('display','none');\r\n";
+  out += "}\r\n";
+  out += "});\r\n";
+  out += "$(function(){\r\n";
+  out += "var handle = $(\"#custom-handle\");\r\n";
+  out += "$(\"#slider\").slider({\r\n";
+  out += "range: \"max\",\r\n";
+  out += "min: 0,\r\n";
+  out += "max: 255,\r\n";
+  out += "value: ";
+  if (httpServer.arg("BRILLO") != ""){
+    out += httpServer.arg("BRILLO");
+    out += ",\r\n";
+  } else {
+    out += "255,\r\n";
+  }
+  out += "create: function(){\r\n";
+  out += "handle.text($(this).slider(\"value\"));\r\n";
+  out += "},\r\n";
+  out += "slide: function(event, ui){\r\n";
+  out += "handle.text(ui.value);\r\n";
+  out += "$(\"#valorBrillo\").val(ui.value);\r\n";
+  out += "}\r\n";
+  out += "});\r\n";
+  out += "});\r\n";
+  out += "function processUrl(color){\r\n";
+  out += "$('a[href*=\"LED\"]').attr('href', function(i,href){\r\n";
+  out += "var valorSlider = document.getElementById(\"valorBrillo\");\r\n";
+  out += "if(color==\"FFFFFF\" && valorSlider.value >= \"128\"){\r\n";
+  out += "valorSlider.value = \"127\";\r\n";
+  out += "} else if (color==\"000000\"){\r\n";
+  out += "valorSlider.value = \"0\";\r\n";
+  out += "}\r\n";
+
+  out += "this.href += \"&BRILLO=\" + valorSlider.value;\r\n";
+  //out += "alert(\"Procesando Url. Brillo: \" + valorSlider.value);\r\n";
+  out += "document.getElementById(\"form1\").submit();\r\n";
+  out += "});\r\n";
+  out += "}\r\n";
+  out += "function AjustBrillo(valor){\r\n";
+  out += "var valorSlider = document.getElementById(\"valorBrillo\");\r\n";
+  out += "valorSlider.value = valor;\r\n";
+  out += "alert('Se ajusta el Brillo a: ' + valorSlider.value)\r\n";
+  out += "}\r\n";
+  out += "</script>\r\n";
+  
+  out += "<title>Wifi light</title>\r\n</head>\r\n";
+  out += "<body style='background-color:#2e2e2e'>\r\n";
+  out += "<span style='display:block; width:100%; font-size:2em; font-family:Verdana; margin:20px 0 0; text-align:center; color:white'>Control Leds ESP8266-Hyperion</span><br/>\r\n";
+  out += "<FORM id=\"form1\" action=\"/\" method=\"post\">\r\n";
+  out += "<p>\r\n";
+  out += "<input id=\"valorBrillo\" maxlength=\"3\" name=\"BRILLO\" type=\"hidden\">\r\n";
+  out += "<input name=\"LED\" type=\"hidden\" id=\"color_value\" value=\"FFFFFF\">\r\n";
+  out += "<button title=\"Abrir el Selector de Color.\" style=\"width:auto; height:10%; font-size:33px; padding:0 3%\" class=\"jscolor {valueElement: 'color_value', width:800, height:500, position:'bottom', borderColor:'#FFF', insetColor:'#FFF', backgroundColor:'#666'}\">Selecciona un color</button>\r\n"; //onchange=\"sendDac(document.getElementById('pin1').value);
+  out += "</p>\r\n";
+  out += "<a href='/?TV=1' title=\"Activar Hyperion\"><span style=\"width:auto; height:auto; background-color:buttonface; color:buttontext; position:absolute; bottom:12%; left:10px; font-size:33px; padding:3%\">TV</span></a><br/>\r\n";
+  out += "<div style=\"background-color:black; border:solid 2px white; width:143px; height:auto; position:absolute; bottom:24%; color:white; padding:1% .5%; right:10; text-align:center; font-size:1.7em;\">\r\n";
+  out += "<p style=\"margin:0 0 10px 0;\">Reset Brillo:</p>\r\n";
+  out += "<p class=\"boton\" onclick=\"AjustBrillo(0);\" title=\"Resetear Brillo a: 0\" style=\"width:45%; height:auto; background-color:buttonface; color:buttontext; font-size: 50px; text-align:center; margin:0 10px 0 0; display:inline-block; float:left;\">-</p>\r\n";
+  out += "<p class=\"boton\" onclick=\"AjustBrillo(255);\" title=\"Resetear Brillo a: 255\" style=\"width:45%; height:auto; background-color:buttonface; color:buttontext; font-size: 50px;text-align:center; margin:0; display:inline-block; float:right;\">+</p>\r\n";
+  out += "</div>\r\n";
+  out += "<input class=\"boton\" type=\"submit\" onclick=\"processUrl(document.getElementById('color_value').value);\" style=\"width:auto; height:10%; position:absolute; bottom:12%; right:10px; font-size:33px; padding:0 3%;\">\r\n";
+  //out += "<INPUT type=\"reset\" style=\"width:200px; height:100px; position:absolute; bottom:12%; right:10px; font-size:33px\">\r\n";
+  out += "<p style=\"position:absolute; font-size:45px; color:white; bottom:14%; left:43%\">Brillo: \r\n";
+  out +=  httpServer.arg("BRILLO");
+  out += "</p>\r\n";
+  out += "<div id=\"slider\"><div id=\"custom-handle\" class=\"ui-slider-handle\"></div></div>\r\n";
+  out += "</form><br/><br/>\r\n";
+
+  out += "<p class=\"Args\" style=\"color:white; position:absolute; top:3%; right:10px; font-size:1.7em; background-color:black; padding:1%; border:solid 2px white;\">Argumentos: ";
+  out += httpServer.args();
+  out += "<br/>\r\n";
+  for ( uint8_t i = 0; i < httpServer.args(); i++ ) {
+    out += "<br/>" + httpServer.argName ( i ) + ": " + httpServer.arg ( i ) + "\r\n";
+  }
+  out += "</p>\r\n";
+  
+  out += "<a href='/?LED=FFFFFF' class=\"envioColor\" onclick=\"processUrl('FFFFFF');\"><span style='display:inline-block; background-color:white; width:20%; height:10%; position:absolute; bottom:0;left:0;'></span></a><br/>\r\n"; // Blanco
+  out += "<a href='/?LED=FF0000' class=\"envioColor\" onclick=\"processUrl('FF0000');\"><span style='display:inline-block; background-color:red; width:20%; height:10%; position:absolute; bottom:0;left:20%;'></span></a><br/>\r\n"; // Rojo
+  out += "<a href='/?LED=00FF00' class=\"envioColor\" onclick=\"processUrl('00FF00');\"><span style='display:inline-block; background-color:green; width:20%; height:10%; position:absolute; bottom:0;left:40%'></span></a><br/>\r\n"; // Verde
+  out += "<a href='/?LED=0000FF' class=\"envioColor\" onclick=\"processUrl('0000FF');\"><span style='display:inline-block; background-color:blue; width:20%; height:10%; position:absolute; bottom:0;left:60%'></span></a><br/>\r\n"; // Azul
+  out += "<a href='/?LED=000000' class=\"envioColor\" onclick=\"processUrl('000000');\"><span style='display:inline-block; background-color:black; width:20%; height:10%; position:absolute; bottom:0;left:80%'></span></a>\r\n"; // Negro
+
+  out += "</body>\r\n";
+  out += "</html>\r\n";
+
+  if (httpServer.hasArg("BRILLO") || httpServer.hasArg("LED")) {
+    // La asignación del Brillo ahora se procesa desde la parte cliente con JQuery, liberando el trabajo al microcontrolador.
+    // Por lo que se comentan las siguientes líneas.
+    /* if (httpServer.arg("BRILLO") == "") {
+      BRIGHTNESS = "255";
+    } else if (httpServer.arg("LED") == "ffffff" && httpServer.arg("BRILLO") >= "128") {
+      BRIGHTNESS = "127"; // Correción de Brillo en el color Blanco para ajustar el consumo de (mA). 82 leds *60ma = 4920ma / 2 = 2460ma. 
+    } else if (httpServer.arg("LED") == "000000" && httpServer.arg("BRILLO") >= "1") {
+      BRIGHTNESS = "0";
+    } else {
+      BRIGHTNESS = httpServer.arg("BRILLO");
+    }*/
+    BRIGHTNESS = httpServer.arg("BRILLO");
+    LEDvalue = httpServer.arg("LED");
+    pixeles.begin();
+  } else if (httpServer.hasArg("TV")) {
+    if ((httpServer.arg("TV")) == "1") {
+      strip.Begin();
+      strip.Show();
+    }
+  }
+  httpServer.send ( 200, "text/html", out );
+}
+
+void handleNotFound() {
+    String message = "File Not Found\n\n";
+    message += "URI: ";
+    message += httpServer.uri();
+    message += "\nMethod: ";
+    message += ( httpServer.method() == HTTP_GET ) ? "GET" : "POST";
+    message += "\nArguments: ";
+    message += httpServer.args();
+    message += "\n";
+    for ( uint8_t i = 0; i < httpServer.args(); i++ ) {
+        message += " " + httpServer.argName ( i ) + ": " + httpServer.arg ( i ) + "\n";
+    }
+    message += "\n";
+    httpServer.send ( 404, "text/plain", message );
+}
 
 void loop()
 {
@@ -371,8 +566,15 @@ void loop()
   handle_raw_port();
   handle_ascii_port();
 
+  if(LEDvalue.length() == 6) { // == 6 si es RGB/ == 8 si es RGBW
+    uint32_t couleur = pixeles.Color(LEDvalue);
+    for(int i=0; i<PixelCount; i++){
+      pixeles.setPixelColor(i, couleur);
+    }
+    pixeles.setBrightness(BRIGHTNESS.toInt());
+    pixeles.show();
+  }
 }
-
 
 void handle_raw_port() {
 
@@ -411,9 +613,7 @@ void handle_raw_port() {
   }
 }
 
-
-
-void   handle_ascii_port() {
+void handle_ascii_port() {
   if (!ASCIIudp) {
     Serial.println("RE-Starting UDP");
     ASCIIudp.begin(ASCIIPort);
@@ -554,7 +754,6 @@ byte ascii2hex(char asc) {
   }  
 }
 
-
 int splitString(char separator, String input, String results[], int numStrings) {
     int idx;
     int last_idx=0;
@@ -635,7 +834,6 @@ void send_bmp() {
     }
   }
 
-
   // create padding (based on the number of pixels in a row
   unsigned char bmpPad[rowSize - 3*w];
   for (int i=0; i<sizeof(bmpPad); i++) {         // fill with 0s
@@ -676,7 +874,5 @@ void send_bmp() {
       bmpfile += bmpPad[x];                 // and ROW padding as needed
     }
   }
-
   httpServer.send(200, "image/bmp", bmpfile);
-
 }
